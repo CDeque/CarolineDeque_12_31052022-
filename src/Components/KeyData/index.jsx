2549ff9d0334f135d4c4c0d55d1cdfd7a16e4e2b
Dequé <carolinedeque@gmail.com>
2022-06-09 19:19:20 +0200

    Ajout Section KeyData

---
+++ src/Components/KeyData/index.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+import colors from "../../utils/style/colors"
+
+const KeyDataContainer= styled.div`
+background-color: ${colors.lightGrey};
+border-radius: 5px;
+width: 258px;
+height:124px;
+display:flex;
+justify-content: space-around;
+align-items: center;
+`
+
+const IconContainer= styled.div`
+width:60px;
+height:60px;
+display:flex;
+justify-content: center;
+align-items: center;
+border-radius:5px;
+&.calories{
+    background-color: ${colors.lightRed}
+}
+&.proteins{
+    background-color: ${colors.lightBlue}
+}
+&.glucids{
+    background-color: ${colors.lightYellow}
+}
+&.lipids{
+    background-color: ${colors.lightPink}
+}
+`
+const KeydataNumber= styled.p`
+font-size:20px;
+font-weight:700;
+color: #282D30;
+`
+const KeydataText= styled.p`
+font-size:14px;
+font-weight:500;
+color: #74798C;
+`
+export default function KeyData({img,number,text, className}){
+
+
+    return(
+
+        <KeyDataContainer className="keydata_container">
+            <IconContainer className={className}><img src={img} alt="keyData icon" /></IconContainer>
+            <div>
+                <KeydataNumber>{number}</KeydataNumber>
+                <KeydataText>{text}</KeydataText>
+            </div>
+        </KeyDataContainer>
+    )
+}
