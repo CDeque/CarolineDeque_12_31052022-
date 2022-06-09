87e116df6ca999819adbbd74b7ad1e89befa0711
Dequé <carolinedeque@gmail.com>
2022-06-09 19:16:52 +0200

    initialisation du projet

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Header from './Components/Header';
+import GlobalStyle from "./utils/style/GlobalStyle"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <GlobalStyle/>
+      <Header/>
+      <Routes>
+        <Route exact path= "/" element={<Home />}/>
+      </Routes>
+    </Router>
+    
+  </React.StrictMode>
+);
+
+
