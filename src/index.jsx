a4c5b628478671272e0640a71c4726e339d6d9f9
Dequé <carolinedeque@gmail.com>
2022-07-05 08:18:05 +0200

    Resolution pb rendering charts switch to ReactV17

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import Header from './Components/Header';
-import GlobalStyle from "./utils/style/GlobalStyle"
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from "./Components/Header";
+import GlobalStyle from "./utils/style/GlobalStyle";
+
+ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle/>
-      <Header/>
+      <GlobalStyle />
+      <Header />
       <Routes>
-        
-        <Route   path= "/" element={<Navigate to={"user/12"}replace/>}/>
-        <Route   path= "user/:userId" element={<Home />}/>
-   
-       
+        <Route path="/" element={<Navigate to={"user/12"} replace />} />
+        <Route path="user/:userId" element={<Home />} />
       </Routes>
     </Router>
-    
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
